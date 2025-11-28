f6b1220871fcb7ede27cae55b1478c85981c6767
Luispmv <luispmv07@hotmail.com>
2025-11-28 14:31:25 -0600

    Agregando versionamiento al bucket de S3 para ello tenemos que llamar a un nuevo recurso de AWS que esta relacionado a S3 el versioning

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aws_s3_bucket" "aws_s3_resource" {
       Name = "terraform_s3_bucket"
     }
 }
+
+// Agregando versionamiento al bucekt de S3
+resource "aws_s3_bucket_versioning" "aws_s3_resource" {
+    bucket = aws_s3_bucket.aws_s3_resource.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
