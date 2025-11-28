b0b00b110ea5fe92be86e81a667bc06f8d401e90
Luispmv <luispmv07@hotmail.com>
2025-11-28 16:00:35 -0600

    Añadiendo una politica de bucket que evita que los usuarios eliminen los objetos de S3 si no tienen MFA activado

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 // Creacion de un bucket de S3
-
 resource "aws_s3_bucket" "aws_s3_resource" {
     bucket = "lpmv-77802" // Aqui definimos el nombre real del bucket de S3.
     // Añadimos etiquetas para identificar el proyecto en AWS.
@@ -15,4 +14,39 @@ resource "aws_s3_bucket_versioning" "aws_s3_resource" {
     versioning_configuration {
       status = "Enabled"
     }
+}
+
+// Imprimimos el arn del bucket
+output "lpmv-77802" {
+    description = "ARN del bucket S3"
+    value = aws_s3_bucket.aws_s3_resource.arn
+}
+
+// Autenticacion por MFA
+resource "aws_s3_bucket_policy" "aws_s3_resource" {
+    bucket = aws_s3_bucket.aws_s3_resource.id
+
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+        Sid       = "DenyObjectDeletionWithoutMFA"
+        Effect    = "Deny"
+        Principal = "*"
+        Action = [
+            "s3:DeleteObject",
+            "s3:DeleteObjectVersion"
+        ]
+        Resource = "${aws_s3_bucket.aws_s3_resource.arn}/*"
+        Condition = {
+            Bool = {
+            "aws:MultiFactorAuthPresent" = "false"
+            }
+            StringEquals = {
+            "aws:PrincipalType" = "User"  # Solo aplica a usuarios IAM (no roles)
+            }
+        }
+        }
+    ]
+    })
 }
