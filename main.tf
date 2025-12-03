94fa125238d10631878827349434dfc5c54e209d
Luispmv <luispmv07@hotmail.com>
2025-12-03 09:56:35 -0600

    Creacion de una llave CMK para utilizar en S3, esta se borrara en 7 dias y servira para encriptar y desencriptar, esta llave le adjuntamos permisos sobre S3, por ultimo cambiamos el cifrado predeterminado del bucket utilizando la CMK que hemos creado

--- main.tf
+++ main.tf
@@ -1,3 +1,58 @@
+// Creacion de una llave CMK (Customer Managed Key) en KMS
+resource "aws_kms_key" "aws_cmk_resource" {
+    description = "CMK para utilizar en S3"
+    deletion_window_in_days = 7
+    enable_key_rotation = true
+    key_usage = "ENCRYPT_DECRYPT"
+    tags = {
+      "Name" = "CMK S3" 
+    }
+    policy = data.aws_iam_policy_document.kms_policy.json
+}
+
+// Definiendo una key polici para que S3 pueda utilizar la CMK
+data "aws_iam_policy_document" "kms_policy" {
+  statement {
+    sid = "EnableIAMUserPermissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::945356073292:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowS3UseOfTheKey"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:EncryptionContext:aws:s3:arn"
+      values   = ["arn:aws:s3:::pablo-bucket-seguro"]
+    }
+  }
+}
+
+
 // Creacion de un bucket de S3
 resource "aws_s3_bucket" "aws_s3_resource" {
     bucket = "lpmv-77802" // Aqui definimos el nombre real del bucket de S3.
@@ -21,6 +76,18 @@ output "lpmv-77802" {
     description = "ARN del bucket S3"
     value = aws_s3_bucket.aws_s3_resource.arn
 }
+
+// Cifrado prdeterminado del bucket S3 utilizando la clave CMK creada anteriormente
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws_cmk_encryption_resource" {
+    bucket = aws_s3_bucket.aws_s3_resource.id
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+        kms_master_key_id = aws_kms_key.aws_cmk_resource.arn
+      }
+    }
+}
+
 
 // Autenticacion por MFA
 resource "aws_s3_bucket_policy" "aws_s3_resource" {
